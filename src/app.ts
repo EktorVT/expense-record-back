b1eb85236fbad66451f527ba1937736ddd2803ac
Hector <hector2001rdz@gmail.com>
2023-12-27 11:11:53 -0600

    Feat [ERB-1] add auth to user

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
+import "./database";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use("/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
